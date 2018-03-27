8ad4add54892fa04b7ddba7d02d280d03d1c83d5
Jhonny Cuevas <master223@gmail.com>
2018-03-27 12:58:14 -0400

    pluqins aqreados para manejo de rutas y nuevos css.

--- core/app.js
+++ core/app.js
@@ -1,21 +1,23 @@
 'use strict';
 window.app_version = 1.1;
 var app =angular.module('astorbetsApp', ['astorbetsApp.models',
-        'ngRoute',
+        'ui.router',
         'satellizer',
         'ngMaterial', 
-        'ngMessages'
+        'ngMessages',
+        'pascalprecht.translate',
+        'ngStorage'
 ]);
 var appModels = angular.module('astorbetsApp.models', []);
 
-app.run(['$rootScope', '$timeout', '$location', '$auth',function ($rootScope,$timeout,$location,$auth ) {
+app.run(['$rootScope', '$timeout', '$location','$auth', '$localStorage' ,function ($rootScope,$timeout,$location,$auth,$localStorage ) {
         $rootScope.isAuthenticated = function() {
             return $auth.isAuthenticated();
         };
         $rootScope.cerrarSesion = function() {
             $auth.logout().then(function() {
                 $location.path('/');
-              //  delete $localStorage.user;
+                delete $localStorage.user;
             });
         };
 
