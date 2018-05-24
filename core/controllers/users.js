75ad5776cf142b84a4ef1334241364e9e3872950
Jhonny Cuevas <master223@gmail.com>
2018-05-24 14:13:25 -0400

    mostrar lista de los partidos que se estan juqando en el momento, aqreqado vista olvido de contraeña, ajustes en los listados de trasacciones,ajsutes en la vista del perfil de usuario, ajustes en el reqistro de usuario.

--- core/controllers/users.js
+++ core/controllers/users.js
@@ -1,55 +1,108 @@
 'use strict';
-app.controller('UsersCtrl', function($scope, $rootScope ,$location, $timeout, $q , $auth,$filter, $mdDialog, UsersService) {
-      $scope.name = null;
-      $scope.user = null;
-      $scope.pass = null;
+app.controller('UsersCtrl', function($scope, $rootScope, $localStorage,$location, $timeout, $q , $auth,$filter, $mdDialog, UsersService) {
+    $scope.pb_usr = false;
+    $scope.name  = null;
+    $scope.user  = null;
+    $scope.pass  = null;
 
     $scope.deserr = "";
     $scope.showerror=false;
 
-      $scope.getUser = function(id_) {
+    $scope.getUser = function(id_) {
+        $scope.pb_usr = true; 
         UsersService.getUser(id_).then(function(response){
+            $scope.pb_usr = false; 
             $scope.user = response.data.data;
         }).catch(function(err){
-            console.log(err);
+            $scope.pb_usr = false; 
         });
-      };
+    };
 
     $scope.register = function() {
       if ($scope.loginForm.$valid) {
-          var user = {
-              description: $scope.name,
-              email: $scope.user,
-              password: $scope.pass
-          };
-          UsersService.store(user).then(function(data){
-              var d = data.data;
-              $rootScope.showAlert (d.type, d.message);
-              //$location.path('/');
-              $rootScope.showLogin();
-          }).catch(function(err){
-              console.log("errorr...");
-              $scope.showerror=true;
-              $scope.deserr = err.data.message;
-              console.log(err.data.message);
-          });
-       }else {
+            $scope.pb_usr = true;
+            var user = {
+                description: $scope.name,
+                email: $scope.user,
+                password: $scope.pass
+            };
+            UsersService.store(user).then(function(data){
+                $scope.pb_usr = false; 
+                var d = data;
+                if (data.status==200){
+                    $rootScope.showAlert (d.type, d.message);
+                    $auth.setToken(d.token);
+                    $rootScope.setUserinfo(d.user);
+                    $rootScope.actual_view="views/events.html";
+                    $rootScope.getListGames(1);
+                    $rootScope.getInitvals();
+                }else{
+                    $scope.showerror=true;
+                    $scope.deserr = d.message; 
+                   // $rootScope.showAlert (d.type, d.message);
+                }   
+            }).catch(function(err){
+                $scope.pb_usr = false; 
+                $scope.showerror=true;
+                $scope.deserr = err.data.message;
+            });
+       }else{
+          $scope.pb_usr = false; 
           $scope.showerror=true;
           $scope.deserr = $filter('translate')('requiredallFields');
        }  
-        
-    }
+    };
+
+    $scope.forgot = function() {
+        if ($scope.loginForm.$valid) {
+              $scope.pb_usr = true;
+              var user = {
+                  email: $scope.user
+              };
+              UsersService.lost(user).then(function(data){
+                  $scope.pb_usr = false; 
+                  var d = data.data;
+                  if (data.status==200){
+                       $rootScope.showAlert (d.type, d.message);
+                       $rootScope.showLogin();
+                   }else{
+                       $scope.showerror=true;
+                       $scope.deserr = d.message; 
+                      // $rootScope.showAlert (d.type, d.message);
+                   }   
+              }).catch(function(err){
+                  $scope.pb_usr = false; 
+                  $scope.showerror=true;
+                  $scope.deserr = err.data.message;
+              });
+         }else{
+            $scope.pb_usr = false; 
+            $scope.showerror=true;
+            $scope.deserr = $filter('translate')('requiredallFields');
+         }  
+     };
 
     $scope.update = function() {
-        $scope.user.image = $scope.file ? 'data:' + $scope.file.filetype + ';base64,' + $scope.file.base64 : '';
+        $scope.pb_usr = true; 
+        //$scope.user.image = $scope.file ? 'data:' + $scope.file.filetype + ';base64,' + $scope.file.base64 : '';
         UsersService.update($scope.user).then(function(data){
             var d = data.data;
-            $rootScope.showAlert (d.type, d.message);
-            $rootScope.showProfile();
+            if (data.status==200){
+                $rootScope.setUserinfo(d.item);
+                $rootScope.showAlert (d.type, d.message);
+            }else{
+                $scope.showerror=true;
+                $scope.deserr = d.message; 
+            }    
+           // $rootScope.showProfile();
+            $scope.pb_usr = false; 
         }).catch(function(err){
+            $scope.pb_usr = false; 
             $scope.showerror=true;
             $scope.deserr = err.message;
         });
     };
+
+
 
 })
